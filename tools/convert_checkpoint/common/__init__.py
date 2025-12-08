0b20db34a17a4aec625d2713463e5ea788022e5e
pengxiangyu <pengxiangyu@baidu.com>
2025-12-08 19:57:02 +0800

    aiak-train-1459 checkpoint转换功能代码重构
    Change-Id: If21a2d1b748cdb1889bb4facc67efe49f44ac1ac

--- tools/convert_checkpoint/common/__init__.py
+++
@@ -1,8 +0,0 @@
-""" convert_checkpoint """
-from common.common_config import CommonConfig
-
-from huggingface.huggingface_checkpoint import HuggingFaceCheckpoint
-from huggingface.huggingface_config import HuggingFaceConfig
-
-from mcore.mcore_checkpoint import McoreCheckpoint
-from mcore.mcore_config import McoreConfig
